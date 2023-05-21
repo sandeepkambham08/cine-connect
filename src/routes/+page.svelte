9bfc959aa6a68af176778d75c3c2a867f2ca1811
sandeep <sandeepkambham08@gmail.com>
2023-05-21 10:25:32 -0400

    💄 Add minimal styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,55 @@
-<h1 class="bg-red-500">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div class="flex flex-col bg-contrast text-on-contrast pt-16 h-screen">
+  <div
+    class="flex flex-col h-fit m-4 bg-base text-on-base rounded-lg shadow-lg p-6"
+  >
+    <p class="text-3xl font-bold text-center text-gray-900 mb-8">
+      This is a multi-platform setup for
+      <span class="text-blue-500">any screen</span>
+    </p>
+    <p class="text-3xl font-bold text-gray-900 mb-4">It uses:</p>
+    <ul class="space-y-2">
+      <li class="flex items-center">
+        <span class="w-4 h-4 rounded-full bg-orange-500 mr-2" />
+        <a
+          href="https://kit.svelte.dev/"
+          target="_blank"
+          class="font-semibold text-orange-500 hover:underline">SvelteKit</a
+        > &nbsp; as a framework
+      </li>
+      <li class="flex items-center">
+        <span class="w-4 h-4 rounded-full bg-blue-500 mr-2" />
+        <a
+          href="https://capacitorjs.com/"
+          target="_blank"
+          class="font-semibold text-blue-500 hover:underline">Capacitor</a
+        >&nbsp; as a cross-platform tool
+      </li>
+      <li class="flex items-center">
+        <span class="w-4 h-4 rounded-full bg-yellow-500 mr-2" />
+        <a
+          href="https://tailwindcss.com/"
+          target="_blank"
+          class="font-semibold text-yellow-500 hover:underline">Tailwind CSS</a
+        >&nbsp; as a CSS framework
+      </li>
+      <li class="flex items-center">
+        <span class="w-4 h-4 rounded-full bg-green-500 mr-2" />
+        <a
+          href="https://vitejs.dev/"
+          target="_blank"
+          class="font-semibold text-green-500 hover:underline">Vite</a
+        >&nbsp; as a bundler
+      </li>
+    </ul>
+  </div>
+  <p
+    class="h-fit m-4 bg-base text-on-base rounded-lg shadow-lg p-6 text-center mt-6"
+  >
+    Liked this setup? Fork the repository from
+    <a
+      href="https://github.com/sandeepkambham08/multi-platform-boilerplate"
+      target="_blank"
+      class="text-blue-500 font-semibold hover:underline">GitHub</a
+    >.
+  </p>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  presets: [require('./tailwind.theme.cjs')],
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {}
