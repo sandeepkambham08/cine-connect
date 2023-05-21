1f0e5129ee8e2ea79641983d64be3f063f5e031d
sandeep <sandeepkambham08@gmail.com>
2023-05-20 20:19:13 -0400

    ➕ add tailwindcss

---
+++ tailwind.theme.cjs
@@ -0,0 +1,34 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem'
+        }
+      },
+      colors: {
+        base: colors.slate[100],
+        contrast: colors.slate[800],
+        danger: { container: colors.rose[200], DEFAULT: colors.rose[500] },
+        muted: colors.zinc[400],
+        primary: { container: colors.red[400], DEFAULT: colors.sky[500] },
+        surface: colors.slate[200],
+        on: {
+          base: colors.slate[900],
+          contrast: colors.slate[100],
+          danger: { container: colors.rose[600], DEFAULT: colors.rose[50] },
+          muted: colors.zinc[50],
+          primary: { container: colors.sky[700], DEFAULT: colors.sky[50] },
+          surface: colors.slate[800]
+        }
+      },
+      fontSize: {
+        '2xs': '.5em'
+      }
+    }
+  }
+};
